4ae6da9d671b0b8218e365c2042068182857f31f
Andrew Sherman <andrew@ohair-sherman.com>
2016-05-30 18:43:41 -0700

    add a partition test

--- partition/partition_test.go
+++ partition/partition_test.go
@@ -8,8 +8,17 @@ func TestSimple(t *testing.T) {
 	for i, v := range output {
 		fmt.Printf("at %v is %v", i, v)
 	}
-
-
+	var expected = []Pair  {
+		Pair{"a", "dnrew"},
+		Pair{"n", "adrew"},
+		Pair{"d", "anrew"},
+		Pair{"r", "adnew"},
+		Pair{"e", "adnrw"},
+		Pair{"w", "adnre"},
+	}
+	 if len(expected) != len(output){
+		 t.Errorf("lengths differ")
+	 }
 	//expected := "andrew"
 	//if output != expected {
 	//	errorString := fmt.Sprintf("Expected %v, got %v", expected, output)

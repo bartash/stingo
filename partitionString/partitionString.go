5413b3cc15759eb744c4ab0a510663ba613be426
Andrew Sherman <andrew@ohair-sherman.com>
2016-06-26 11:49:51 -0700

    start to do some math

--- partitionString/partitionString.go
+++ partitionString/partitionString.go
@@ -1,16 +1,30 @@
 package partitionString
 
+import (
+	"math"
+)
+
 type Pair struct {
 	first, second string
 }
 
 type PartitionString struct {
-	N int
-	S string
+	n    int
+	seed string
 }
 
 
 func (p PartitionString) Next() *Pair {
-	return new(Pair)
+	pair := new(Pair)
+	max := int(math.Pow(2, float64(len(p.seed)))) // should be done in "constructor"
+	if p.n > max {
+		// need to check for off by one and other logic errors
+		return nil
+	}
+
+	// treat n as a binary number to decide which characters of seed are in first string of pair
+
+	p.n++
+	return pair
 }
 

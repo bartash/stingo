6bad39b774905fe721d638564fef22a4865191b5
Andrew Sherman <andrew@ohair-sherman.com>
2017-01-04 12:59:58 -0800

    Validate PartitionString on "and"

--- partitionString/partitionString_test.go
+++ partitionString/partitionString_test.go
@@ -1,19 +1,42 @@
 package partitionString
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestPartitionString(t *testing.T) {
-	second := NewPartitionString("and")
-	pair := second.Next()
+	partitionString := NewPartitionString("and")
+	expected := map[Pair]bool{
+		*NewPair("a", "nd"): true,
+		*NewPair("n", "ad"): true,
+		*NewPair("an", "d"): true,
+		*NewPair("ad", "n"): true,
+		*NewPair("nd", "a"): true,
+		*NewPair("d", "an"): true,
+		*NewPair("and", ""): true,
+	}
+	fmt.Printf("length of expect = %d\n", len(expected))
+	for key, value := range expected {
+		fmt.Print(key, "=", value)
+	}
+	pair := partitionString.Next()
+	var count int
 	for pair != nil {
-		pair = second.Next();
+		pair = partitionString.Next()
 		if pair != nil {
+			count++
 			fmt.Printf("got pair '%v' & '%v'\n", pair.First, pair.Second)
+			val, ok := expected[*pair]
+			if !ok {
+				t.Errorf("unexpected pair %+v %+v", pair, val)
+			}
 		} else {
 			fmt.Printf("got nil return\n")
 		}
 	}
+	if count != len(expected) {
+		t.Errorf("unexpected count %d differs from len(expected)", count, len(expected))
+	}
+
 }
